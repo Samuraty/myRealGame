35a9978b48b8c9271b971b61b2aec4a6e18e0e27
Samuraty <alexandra.garciagasco@gmail.com>
2018-06-28 16:56:24 +0200

    Ordenado

--- js/sound.js
+++ js/sound.js
@@ -3,17 +3,17 @@ var ctx = canvas.getContext("2d");
 
 function Sounds() {
   // BACKGROUND MUSIC
-  this.backgroundAudio = new Audio("./Music/Candies.mp3");
+  this.backgroundAudio = new Audio("./Music/Music.mp3");
   this.backgroundAudio.loop = true;
-  this.backgroundAudio.volume = .25;
+  this.backgroundAudio.volume = .2;
   this.backgroundAudio.load();
   // COLLISION EFFECT
   this.gameoverAudio = new Audio("./Music/Explosion.mp3");
-  this.gameoverAudio.volume = .75;
+  this.gameoverAudio.volume = .7;
   this.gameoverAudio.load();
   // COIN EFFECT
-  this.coinsAudio = new Audio("./Music/Coins.mp3");
-  this.coinsAudio.volume = .25;
+  this.coinsAudio = new Audio("./Music/Candies.mp3");
+  this.coinsAudio.volume = 1;
   this.coinsAudio.load();
 }
 
